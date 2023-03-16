281d63ed13f0c7998d62f10046fd77f3b7619080
emresvd <109899306+emresvd@users.noreply.github.com>
2023-03-16 20:47:45 +0300

    modified:   requirements.txt	main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import shutil
+import os
+
+if sys.argv[-1] == "clean":
+    for i in ["build", "dist", "funcbygpt.egg-info", "files"]:
+        try:
+            shutil.rmtree(i)
+        except FileNotFoundError:
+            pass
+
+if sys.argv[-1] == "build":
+    os.system("python setup.py sdist bdist_wheel")
+
+if sys.argv[-1] == "upload":
+    os.system("twine upload dist/*")
